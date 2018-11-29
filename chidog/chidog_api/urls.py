a544f53f939ef8c8cdb2230820d6b2877eab8753
Chris Roman <Christopherroman85@gmail.com>
2018-11-29 16:02:33 -0600

    created reply routes and added urls

--- chidog/chidog_api/urls.py
+++ chidog/chidog_api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import Pets
+from .views import Pets, Pet_Detail, Posts, Post_Detail, Replies, Reply_Detail
 
 urlpatterns = [
-	path('', Pets.as_view())
+	path('pets/', Pets.as_view()),
+	path('posts/', Posts.as_view()),
+	path('reply/', Replies.as_view()),
+	path('pets/<int:pk>', Pet_Detail.as_view()),
+	path('posts/<int:pk>', Post_Detail.as_view()),
+	path('reply/<int:pk>', Reply_Detail.as_view())
 ]
--- chidog/chidog_api/views.py
+++ chidog/chidog_api/views.py
@@ -6,7 +6,11 @@ import json
 
 # Create your views here.
 
+# Pet Views and Routes for the pets that users can add to their profile.
+
 class Pets(View):
+
+	# Pet Get Route
 	def get(self, request):
 		if(request.user.is_authenticated):
 			user = User.objects.get(id=request.user.id)
@@ -19,6 +23,7 @@ class Pets(View):
 				'data': pet_list,
 				}, safe=False)
 
+	# Pet Post Route
 	def post(self, request):
 		data = request.body.decode('utf-8')
 		data = json.loads(data)
@@ -32,10 +37,13 @@ class Pets(View):
 			return JsonResponse({'error', 'Data not valid.'}, safe=False)
 
 class Pet_Detail(View):
+
+	# Pet Detail view Get Route
 	def get(self, request, pk):
 		pet_list = list(Pet.object.filter(pk=pk).values)
 		return JsonResponse({'data': pet_list}, safe=False)
 
+	# Pet Detail View Edit Route
 	def put(self, request, pk):
 		data = request.body.decode('utf-8')
 		data = json.loads(data)
@@ -62,6 +70,7 @@ class Pet_Detail(View):
 		except:
 			return JsonResponse({'error': 'Something went wrong.'}, safe=False)
 
+	# Pet Delete Route
 	def delete(self, request, pk):
 		try:
 			pet_to_delete = Pet.object.get(pk=pk)
@@ -70,11 +79,134 @@ class Pet_Detail(View):
 		except:
 			return JsonResponse({'data': 'Removal failed. Please try again.'})
 
-# class Posts(View):
-# 	def get(self, request):
-		
+# These are the Views and Routes for the Posts that users can make. 
+
+class Posts(View):
+
+	# Posts Get Route
+	def get(self, request):
+		if(request.user.is_authenticated):
+			user = User.object.get(id=request.user.id)
+			post_list = list(user.post.all().values())
+		return JsonResponse({
+			'Content-Type': 'application/json',
+			'credentials': 'include',
+			'status': 200,
+			'data': post_list
+			}, safe=False)
+
+		# Posts Post Route
+		def post(self, request):
+			data = request.body.decode('utf-8')
+			data = json.loads(data)
+
+			try:
+				new_post = Post(post_body=data['post_body'])
+				new_post.created_by = request.user
+				new_post.save()
+				data['id'] = new_post.id
+				return JsonResponse({'data': data}, safe=False)
+			except:
+				return JsonResponse({'error': 'Data not valid'}, safe=False)
+
+class Post_Detail(View):
+
+	# Post Detail Get Route
+	def get(self, request, pk):
+		post_list = list(Post.object.filter(pk=pk).values)
+		return JsonResponse({'data': post_list}, safe=False)
+
+	# Posts Detail Edit Route
+	def put(self, request, pk):
+		data = request.body.decode('utf-8')
+		data = json.loads(data)
+		try:
+			edit_post = Post.objects.get(pk=pk)
+			data_key = list(data.keys())
+
+			for key in data_key:
+				if key == 'post_body':
+					edit_post.post_body = data[key]
+			edit_post.save()
+			data['id'] = edit_post.id
+			return JsonResponse({'data': data}, safe=False)
+		except Post.DoesNotExist:
+			return JsonResponse({'data': 'Post does not exist.'})
+		except:
+			return JsonResponse({'data': 'Something went wrong.'}, safe=False)
+
+	# Post Delete Route
+	def delete(self, request, pk):
+		try:
+			post_to_delete = Post.object.get(pk=pk)
+			post_to_delete.delete()
+			return JsonResponse({'data': 'Delete successful'})
+		except:
+			return JsonResponse({'data': 'Delete failed.'})
 
 
+# These are the Views and Routes for Replies that users can create.
 
+class Replies(View):
 
+	# Reply Get Route
+	def get(self, request):
+		if(request.user.is_authenticated):
+			user = User.objects.get(id=request.user.id)
+			reply_list = list(user.replies.all().values())
+		return JsonResponse({
+			'Content-Type': 'application/json',
+			'credentials': 'include',
+			'status': 200,
+			'data': reply_list
+			}, safe=False)
+
+	# Reply Post Route
+	def post(self, request, pk):
+		data = request.body.decode('utf-8')
+		data = json.loads(data)
+
+		try:
+			new_reply = Reply(reply_body=data['reply_body'])
+			new_reply.replied_to = req.params.pk
+			new_reply.created_by = request.user
+			data['id'] = new_reply
+			return JsonResponse({'data': data}, safe=False)
+		except:
+			return JsonResponse({'error': 'Data not valid'}, safe=False)
+
+class Reply_Detail(View):
+
+	# Reply detail Get Route
+	def get(self, request,pk):
+		reply_list = list(Reply.object.filter(pk=pk).values)
+		return JsonResponse({'data': reply_list}, safe=False)
+
+	# Reply Edit Route
+	def put(self, request, pk):
+		data = request.body.decode('utf-8')
+		data = json.loads(data)
+
+		try:
+			edit_reply = Reply.objects.get(pk=pk)
+			data_key = list(data.keys())
+
+			if key == 'reply_body':
+				edit_reply.reply_body = data[key]
+			edit_reply.save()
+			data['id'] = edit_reply.id
+			return JsonResponse({'data': data}, safe=False)
+		except Reply.DoesNotExist:
+			return JsonResponse({'error': 'Reply does not exist.'})
+		except:
+			return JsonResponse({'error': 'Something went wrong.'}, safe=False)
+
+	#Reply Delete Route
+	def delete(self, request, pk):
+		try:
+			reply_to_delete = Reply.objects.get(pk=pk)
+			reply_to_delete.delete()
+			return JsonResponse({'data': 'Delete successful.'})
+		except:
+			return JsonResponse({'data': 'Something went wrong.'})
 
