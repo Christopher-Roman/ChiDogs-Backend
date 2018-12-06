df7831b5e6cb0c6b12b8e7a24847930baa37ce17
Chris Roman <Christopherroman85@gmail.com>
2018-12-05 22:09:12 -0600

    trying another route to fix cors

--- chidog/settings.py
+++ chidog/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = ['https://chidog.herokuapp.com/register']
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
-    HOST,
+    LOCAL_HOST,
 )
 
 
