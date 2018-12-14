9af7a50501c0de7a2ba74e336bf6afdbfecf31f1
Chris Roman <Christopherroman85@gmail.com>
2018-12-14 13:42:47 -0600

    get all posts route works

--- accounts_api/urls.py
+++ accounts_api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import Create_User, Authentication, User_Detail, logout, getToken
+from .views import Create_User, Authentication, User_Detail, logout, getToken, Post_Detail
 
 urlpatterns = [
 	path('', Create_User.as_view()),
 	path('login/', Authentication.as_view()),
 	path('user/', User_Detail.as_view()),
-	path('post/', Post_Detail.as_view())
+	path('post/', Post_Detail.as_view()),
 	path('search/', User_Detail.as_view()),
 	path('logout/', logout),
 	path('getToken', getToken)
